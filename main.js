663657053df47bc0db5f2f7dbd1787fa6e535607
Ciara W <88999595+ciaraweston@users.noreply.github.com>
2021-12-14 19:15:53 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 // Make a 10 card memory game - users must be able to select two cards and check if they are a match.
 // If they are a match, they will stay flipped. If not, they will flip back over. Game is over when all cards are matched and flipped over.
 
-
-// VARIABLES
-
 const characters = document.querySelectorAll('.Card')
 
-// FUNCTIONS
 
 function shuffleCards() {
   let character1 = 'BlackPanther',
@@ -16,8 +12,6 @@ function shuffleCards() {
     character5 = 'Shuri',
     characterNames = [character1, character2, character3, character4, character5]
 
-  //concat will return and merge the character names 
-  //using math.random will return a number between 0-1, while sort returns a    number between -5, assigning each number by the array
 
   characterNames = characterNames.concat(characterNames).sort(() => Math.random() - 0.5)
   characters.forEach((el, i) => {
@@ -27,7 +21,7 @@ function shuffleCards() {
 }
 shuffleCards()
 
-//CHARACTERS
+
 
 let arrImg = [
   'blackpanther.png',
@@ -36,7 +30,7 @@ let arrImg = [
   'okoye.png',
   'shuri.png',
 ]
-//ADD EVENT LISTENER
+
 
 characters.forEach(el => el.addEventListener('click', checkForMatch))
 
@@ -66,35 +60,9 @@ function checkForMatch(click) {
   }
   if (count === 5) {
     setTimeout(() => {
-      alert('Wakada Forever!')
+      alert('WAKANDA FOREVER! L')
     }, 310)
   }
 }
 const reset = document.getElementById('reset');
 reset.addEventListener('click', () => location.reload());
-
-
-
-
-
-
-
-
-// check length of array 
-// if array = two we check if the length is two. 
-// if its two we run a compare function
-// if theyre the same then we disable does two and add one to a counter 
-// when counter is a certain length the game is over 
-
-
-//define a card and set the innerHTML of that card to one of the city names
-    // wrap it in the function
-// Define a function by putting an eventListener on each of the cards
-        // expect that the value will show how many times you clicked on the console
-        // keep track of how many cards we have flipped over
-        // If we have 2 cards flipped over we want to check if they're equal
-        // If they're not equal it would be not a match and the cards will just flip back over
-        // If they are equal they stay flipped
-// Define a check game over function
-        // When there is no more cards the game will be over
-// Write a function to shuffle the cards after
